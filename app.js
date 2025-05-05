1b887c2484bbda6ff223035aa1727e52fb9a8ae7
joaopedroxzk <jotapepeto9@gmail.com>
2025-05-05 15:37:47 -0300

    05-05-25

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require("express");
 
 const app = express();
 
+const sqlite3 = require("sqlite3");
+
+
+// Conexão com o banco de dados
+const db = new sqlite3.Database("users.db");
+
+db.serialize(() => {
+    db.run(
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+    )
+})
+
 app.set("view engine", "ejs");
+
 
 app.use('/static', express.static(__dirname + '/static'));
 
-const sqlite3 = require("sqlite3");
+// Configuração do Express para processar requisições POST com BODY PARAMETERS
+app.use(express.urlencoded({ extended: true })); // versão EXPRESS >= 5.x.x
+
+
 
 app.set('view engine', 'ejs');
 
@@ -31,6 +47,24 @@ app.get("/login", (req, res) => {
     console.log("GET / login");
 });
 
+//Rota /login para processamento dos dados do formulário do LOGIN no cliente
+app.post("/login", (req, res) => {
+    //res.render("pages/sobre");
+    console.log(JSON.stringify(req.body));
+    const { username, password } = req.body;
+
+    // 1. Verificar se o usuário existe
+    const query = "SELECT * FROM users WHERE username=? AND password=?"
+    db.get(query, [username, password], (err) => {
+        if (err) throw err;
+
+        res.redirect("/dashboard")
+    })
+
+
+    console.log("POST / login");
+});
+
 app.get("/dashboard", (req, res) => {
     res.render("pages/dashboard");
     console.log("GET / dashboard");
@@ -38,4 +72,39 @@ app.get("/dashboard", (req, res) => {
 
 app.listen(4000, () => {
     console.log(`Servidor NODEjs ativo na porta 4000`);
+});
+
+app.post("/cadastro", (req, res) => {
+
+    console.log("POST /cadastro")
+    console.log(JSON.stringify(req.body));
+    const { username, password } = req.body;
+
+    const query1 = "SELECT * FROM users WHERE username=?";
+    const query2 = "INSERT INTO users (username, password) VALUES (? , ?)";
+
+    db.get(query1, [username], (err, row) => {
+        if (err) throw err;
+
+        //1. VErificar se o usuário existe
+
+        console.log(JSON.stringify(row))
+        if (row) {
+
+            // 2. Se o usuário existir, negar cadastro
+            console.log(`Usuários: ${username} já cadastrado`)
+            res.send("Usuário já cadastrado");
+        } else {
+            // 3. Se não, fazer o insert
+            db.get(query2, [username, password], (err, row) => {
+
+                if (err) throw err;
+
+                //1. Verificar se o usuário existe
+                console.log(JSON.stringify(row))
+                console.log(`Usuários: ${username} cadastrado com sucesso.`)
+                res.redirect("/login");
+            })
+        }
+    })
 });
