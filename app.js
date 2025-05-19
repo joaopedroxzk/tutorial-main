ae5491da85ec6664218f4e8f6b3bf973a36e4a3b
joaopedroxzk <jotapepeto9@gmail.com>
2025-05-19 16:23:19 -0300

    19/05 final

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.use(express.urlencoded({ extended: true })); // versão EXPRESS >= 5.x.x
 
 app.set('view engine', 'ejs');
 
-app.get("/index", (req, res) => {
+app.get("/", (req, res) => {
     res.render("pages/index", { título: "index", req: req });
-    console.log("GET / index");
+    console.log("GET /");
 });
 
 //Exercício, criar uma rota para a página Sobre
@@ -60,9 +60,27 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-    res.render("pages/login", { título: "login", req: req});
+    res.render("pages/login", { título: "login", req: req });
     console.log("GET / login");
 });
+app.get("/nao-autorizado", (req, res) => {
+    res.render("pages/nao-autorizado", { título: "nao-autorizado", req: req });
+    console.log("GET / nao-autorizado");
+});
+app.get("/usuario-cadastrado", (req, res) => {
+    res.render("pages/usuario-cadastrado", { título: "usuario-cadastrado", req: req });
+    console.log("GET / usuario-cadastrado");
+});
+app.get("/usuario-invalido", (req, res) => {
+    res.render("pages/usuario-invalido", { título: "usuario-invalido", req: req });
+    console.log("GET / usuario-invalido");
+});
+app.get("/cadastrado-sucesso", (req, res) => {
+    res.render("pages/cadastrado-sucesso", { título: "cadastrado-sucesso", req: req });
+    console.log("GET / cadastrado-sucesso");
+});
+
+
 
 //Rota /login para processamento dos dados do formulário do LOGIN no cliente
 app.post("/login", (req, res) => {
@@ -72,11 +90,16 @@ app.post("/login", (req, res) => {
 
     // 1. Verificar se o usuário existe
     const query = "SELECT * FROM users WHERE username=? AND password=?"
-    db.get(query, [username, password], (err) => {
+    db.get(query, [username, password], (err, row) => {
         if (err) throw err;
-        req.session.username = username;
-        req.session.loggedin = true;
-        res.redirect("/dashboard")
+        if (row) {
+            console.log("criando sessão")
+            req.session.username = username;
+            req.session.loggedin = true;
+            res.redirect("/dashboard")
+        } else {
+            res.redirect("/usuario-invalido");
+        }
     })
 
 
@@ -93,49 +116,54 @@ app.get("/dashboard", (req, res) => {
         db.all(query, [], (err, row) => {
             if (err) throw err;
             //renderiza a página dashboard com a lista de usuário coletada do BD pelo SELECT
-            res.render("pages/dashboard", { título: "Tabela de usuário", dados: row, req:req });
+            res.render("pages/dashboard", { título: "Tabela de usuário", dados: row, req: req });
         });
     } else {
-        res.send("Usuário não logado");
+        res.redirect("/nao-autorizado");
     }
 });
 
 
-app.listen(4000, () => {
-    console.log(`Servidor NODEjs ativo na porta 4000`);
-});
-
 app.post("/cadastro", (req, res) => {
-
+    
     console.log("POST /cadastro")
     console.log(JSON.stringify(req.body));
     const { username, password } = req.body;
-
+    
     const query1 = "SELECT * FROM users WHERE username=?";
     const query2 = "INSERT INTO users (username, password) VALUES (? , ?)";
-
+    
     db.get(query1, [username], (err, row) => {
         if (err) throw err;
-
+        
         //1. VErificar se o usuário existe
 
         console.log(JSON.stringify(row))
         if (row) {
-
+            
             // 2. Se o usuário existir, negar cadastro
             console.log(`Usuários: ${username} já cadastrado`)
-            res.send("Usuário já cadastrado");
+            res.redirect("/usuario-cadastrado");
         } else {
             // 3. Se não, fazer o insert
             db.get(query2, [username, password], (err, row) => {
-
+                
                 if (err) throw err;
-
+                
                 //1. Verificar se o usuário existe
                 console.log(JSON.stringify(row))
                 console.log(`Usuários: ${username} cadastrado com sucesso.`)
-                res.redirect("/login");
+                res.redirect("/cadastrado-sucesso");
             })
         }
     })
+});
+
+app.use('/{*erro}', (req, res) => {
+    // Envia uma resposta de erro 404
+    res.status(404).render('./pages/not-found', {título: "Erro 404", req: req});
+});
+
+app.listen(4000, () => {
+    console.log(`Servidor NODEjs ativo na porta 4000`);
 });
